58f6f9946d8de5d1eee919f08724e721144af667
rheuvel <rheuvel@sogyo.nl>
2022-12-12 21:21:59 +0100

    add 31 en 33

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ docker build -t rheuvel/xamarin-android:27-ndk-latest ./27/ndk
 docker build -t rheuvel/xamarin-android:28-ndk-latest ./28/ndk
 docker build -t rheuvel/xamarin-android:29-ndk-latest ./29/ndk
 docker build -t rheuvel/xamarin-android:30-ndk-latest ./30/ndk
+docker build -t rheuvel/xamarin-android:31-ndk-latest ./31/ndk
+docker build -t rheuvel/xamarin-android:31-ndk-latest ./33/ndk
 
 # API Levels
 docker build -t rheuvel/xamarin-android:26-latest ./26
@@ -19,3 +21,5 @@ docker build -t rheuvel/xamarin-android:27-latest ./27
 docker build -t rheuvel/xamarin-android:28-latest ./28
 docker build -t rheuvel/xamarin-android:29-latest ./29
 docker build -t rheuvel/xamarin-android:30-latest ./30
+docker build -t rheuvel/xamarin-android:31-latest ./31
+docker build -t rheuvel/xamarin-android:31-latest ./33
--- push.sh
+++ push.sh
@@ -12,6 +12,8 @@ docker push rheuvel/xamarin-android:27-ndk-latest
 docker push rheuvel/xamarin-android:28-ndk-latest
 docker push rheuvel/xamarin-android:29-ndk-latest
 docker push rheuvel/xamarin-android:30-ndk-latest
+docker push rheuvel/xamarin-android:31-ndk-latest
+docker push rheuvel/xamarin-android:33-ndk-latest
 
 # API Levels
 docker push rheuvel/xamarin-android:26-latest
@@ -19,3 +21,5 @@ docker push rheuvel/xamarin-android:27-latest
 docker push rheuvel/xamarin-android:28-latest
 docker push rheuvel/xamarin-android:29-latest
 docker push rheuvel/xamarin-android:30-latest
+docker push rheuvel/xamarin-android:31-latest
+docker push rheuvel/xamarin-android:33-latest
