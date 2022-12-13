ddacb84fc9134864390e8686d45a27e084edbc0f
rheuvel <rheuvel@sogyo.nl>
2022-12-13 20:30:15 +0100

    updates

--- push.sh
+++ push.sh
@@ -7,19 +7,19 @@ docker push rheuvel/xamarin-android:platform-only
 docker push rheuvel/xamarin-android:ndk-only
 
 # API Levels + NDK
-docker push rheuvel/xamarin-android:26-ndk-latest
-docker push rheuvel/xamarin-android:27-ndk-latest
-docker push rheuvel/xamarin-android:28-ndk-latest
-docker push rheuvel/xamarin-android:29-ndk-latest
-docker push rheuvel/xamarin-android:30-ndk-latest
+#docker push rheuvel/xamarin-android:26-ndk-latest
+#docker push rheuvel/xamarin-android:27-ndk-latest
+#docker push rheuvel/xamarin-android:28-ndk-latest
+#docker push rheuvel/xamarin-android:29-ndk-latest
+#docker push rheuvel/xamarin-android:30-ndk-latest
 docker push rheuvel/xamarin-android:31-ndk-latest
 docker push rheuvel/xamarin-android:33-ndk-latest
 
 # API Levels
-docker push rheuvel/xamarin-android:26-latest
-docker push rheuvel/xamarin-android:27-latest
-docker push rheuvel/xamarin-android:28-latest
-docker push rheuvel/xamarin-android:29-latest
-docker push rheuvel/xamarin-android:30-latest
+#docker push rheuvel/xamarin-android:26-latest
+#docker push rheuvel/xamarin-android:27-latest
+#docker push rheuvel/xamarin-android:28-latest
+#docker push rheuvel/xamarin-android:29-latest
+#docker push rheuvel/xamarin-android:30-latest
 docker push rheuvel/xamarin-android:31-latest
 docker push rheuvel/xamarin-android:33-latest
