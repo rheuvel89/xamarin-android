0e1a1b50882ab52e815ca5146c386a7085a6052d
rheuvel <rheuvel@sogyo.nl>
2024-09-02 21:33:57 +0200

    add api level 34

--- build.sh
+++ build.sh
@@ -12,8 +12,9 @@ docker build -t rheuvel/xamarin-android:ndk-only ./ndk
 #docker build -t rheuvel/xamarin-android:28-ndk-latest ./28/ndk
 #docker build -t rheuvel/xamarin-android:29-ndk-latest ./29/ndk
 #docker build -t rheuvel/xamarin-android:30-ndk-latest ./30/ndk
-docker build -t rheuvel/xamarin-android:31-ndk-latest ./31/ndk
-docker build -t rheuvel/xamarin-android:33-ndk-latest ./33/ndk
+#docker build -t rheuvel/xamarin-android:31-ndk-latest ./31/ndk
+#docker build -t rheuvel/xamarin-android:33-ndk-latest ./33/ndk
+docker build -t rheuvel/xamarin-android:33-ndk-latest ./34/ndk
 
 # API Levels
 #docker build -t rheuvel/xamarin-android:26-latest ./26
@@ -21,5 +22,6 @@ docker build -t rheuvel/xamarin-android:33-ndk-latest ./33/ndk
 #docker build -t rheuvel/xamarin-android:28-latest ./28
 #docker build -t rheuvel/xamarin-android:29-latest ./29
 #docker build -t rheuvel/xamarin-android:30-latest ./30
-docker build -t rheuvel/xamarin-android:31-latest ./31
-docker build -t rheuvel/xamarin-android:33-latest ./33
+#docker build -t rheuvel/xamarin-android:31-latest ./31
+#docker build -t rheuvel/xamarin-android:33-latest ./33
+docker build -t rheuvel/xamarin-android:33-latest ./34
--- push.sh
+++ push.sh
@@ -12,7 +12,8 @@ docker push rheuvel/xamarin-android:ndk-only
 #docker push rheuvel/xamarin-android:28-ndk-latest
 #docker push rheuvel/xamarin-android:29-ndk-latest
 #docker push rheuvel/xamarin-android:30-ndk-latest
-docker push rheuvel/xamarin-android:31-ndk-latest
+#docker push rheuvel/xamarin-android:31-ndk-latest
+#docker push rheuvel/xamarin-android:33-ndk-latest
 docker push rheuvel/xamarin-android:33-ndk-latest
 
 # API Levels
@@ -21,5 +22,6 @@ docker push rheuvel/xamarin-android:33-ndk-latest
 #docker push rheuvel/xamarin-android:28-latest
 #docker push rheuvel/xamarin-android:29-latest
 #docker push rheuvel/xamarin-android:30-latest
-docker push rheuvel/xamarin-android:31-latest
+#docker push rheuvel/xamarin-android:31-latest
+#docker push rheuvel/xamarin-android:33-latest
 docker push rheuvel/xamarin-android:33-latest
